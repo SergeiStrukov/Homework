0dfe7f586af2f212ffe44f8e372f30aef45a9c0f
SergeiStrukov <serega.strukov21@gmail.com>
2024-04-15 09:52:51 +0400

    Сделал 7 методов и создал классы.

--- src/main/java/org/example/_10_04_2024/Information/Person.java
+++ src/main/java/org/example/_10_04_2024/Information/Person.java
@@ -1,31 +1,24 @@
 package org.example._10_04_2024.Information;
 
-import com.github.javafaker.Address;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
 @Getter
-//    5.Создать класс Person.
-//        Поля:
-//        - имя
-//- фамилия
-//- адрес(это отдельный класс)
-//- зарплата
-//- авто(это отдельный класс)
-//- массив счетов(отдельный клас)
+@Setter
 public class Person {
     private String name;
-private String surname;
-private Address address;
-private double salary;
-private Auto auto;
+    private String surname;
+    private Address address;
+    private double salary;
+    private Auto auto;
+    private Account[] accounts;
 
-    public Person(String name, String surname, Address address, double salary, Auto auto) {
+    public Person(String name, String surname, Address address, double salary, Auto auto, Account[] accounts) {
         this.name = name;
         this.surname = surname;
         this.address = address;
         this.salary = salary;
         this.auto = auto;
+        this.accounts = accounts;
     }
 }
