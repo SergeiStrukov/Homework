8eac879d16c9c2ed1a5fe2ef75bcb08221a4e04d
SergeiStrukov <serega.strukov21@gmail.com>
2024-06-12 11:15:05 +0400

    add 1 test

--- src/test/java/_2024_06_10/StudentRegistryTest.java
+++ src/test/java/_2024_06_10/StudentRegistryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static _2024_06_10.StudentRegistry.generateStudents;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,7 +23,7 @@ class StudentRegistryTest {
     void addStudentException() {
         Student student = StudentRegistry.createStudent();
         StudentRegistry.addStudent(student);
-        assertThrows(IllegalArgumentException.class,()->StudentRegistry.addStudent(student));
+        assertThrows(IllegalArgumentException.class, () -> StudentRegistry.addStudent(student));
     }
 
     @Test
@@ -36,14 +37,24 @@ class StudentRegistryTest {
     @Test
     void findStudentsByMajor() {
         StudentRegistry.generateStudents();
-        Map <Integer,Student> students = StudentRegistry.getStudents();
-        List <Student> bStudents = StudentRegistry.findStudentsByMajor("BIOLOGY");
+        Map<Integer, Student> students = StudentRegistry.getStudents();
+        List<Student> bStudents = StudentRegistry.findStudentsByMajor("BIOLOGY");
         assertTrue(bStudents.stream().allMatch(student -> student.getMajor().name().equalsIgnoreCase("BIOLOGY")));
         System.out.println("Students from biology: " + bStudents.size());
     }
 
     @Test
     void calculateAverageGrade() {
+        StudentRegistry.generateStudents();
+        Map<Integer, Student> students = StudentRegistry.getStudents();
+        double grade1 = 4.5;
+        double calcAverageGrade = students.values().stream()
+                .mapToDouble(Student::getGrade)
+                .peek(grade -> System.out.println("Grade: " + grade))
+                .average()
+                .orElse(0.0);
+        assertNotEquals(grade1, calcAverageGrade, "Средняя оценка не должна быть равна 4.5");
+        System.out.println("Calc average grade:" + calcAverageGrade);
     }
 
     @Test
