53ea89ae2a1cbabe7bdcfb1569c46715d17f9e3d
SergeiStrukov <serega.strukov21@gmail.com>
2024-06-20 22:36:27 +0400

    Выполнил ДЗ за 19.06.24

---
+++ src/main/java/New/_19_06_2024/Main.java
@@ -0,0 +1,26 @@
+package New._19_06_2024;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User();
+        UserChecker checkUser = new UserChecker(user);
+
+        try {
+            checkUser.enterName();
+            checkUser.enterSurname();
+            checkUser.enterBirthdayDate();
+            checkUser.enterDescription();
+
+            // users information
+            System.out.println("Name:" + user.getName());
+            System.out.println("Surname:" + user.getSurname());
+            System.out.println("Birthday:" + user.getBirthdayDate());
+            System.out.println("Description:" + user.getDescription());
+        } catch (NameLengthException | SurnameLengthException  | InvalidDateFormatException e
+        ) {
+            System.err.println("Ошибка: " + e.getMessage());
+        }
+    }
+}
